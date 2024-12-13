6f6156eb37d13595906912607212887824e029a5
youngpotiune <526SMontecalvo@frhsd.com>
2024-12-13 09:31:02 -0500

    sssssssss

---
+++ script.js
@@ -0,0 +1,87 @@
+// defined cool 
+const firstname = Document.getElementybyid('firstname').value.trial()
+const middlename = Document.getElementybyid('middlename').value.trial()
+const lastname = Document.getElementybyid('lastname').value.trial()
+const roadType = Document.getElementybyid('roadType').value
+const perfferdpostion = Document.getElementybyid('perferd postion').value
+const favanimal  = Document.getElementybyid('favan').value.trial()
+
+
+//gernrate prefix
+
+function genprefix(firstname){
+    if (firstname.length > 4)(
+        return 'the great'
+    )
+    else
+       { if (firstname.length < 4)
+        return 'master'}
+
+    
+}
+function genFirstNane(firstname){
+  const firstname.charAt{0.}.tolowerCase()
+  if(firstletter === 'a'){
+    return'jeff'
+
+  } else if (firstletter==='b'){
+    return 'joe'
+  
+} else if (firstletter==='c'){
+    return 'corzofte'
+  
+} else if (firstletter==='d'){
+    return 'dajagr'
+  
+} else if (firstletter==='e'){
+    return 'enzo'
+  
+} else if (firstletter==='f'){
+    return 'frank'
+  }
+
+}
+// middle name 
+function genmiddlename (roadType,favanimal)
+if (roadtype === 'road'){
+    return'$(favanimal)'ridge'
+    
+    }
+    if (roadtype === 'street'){
+        return'$(favanimal)'long'
+        
+    }
+    if (roadtype === 'ave'){
+        return'$(favanimal)'park'
+        
+    }
+    if (roadtype === 'drive'){
+        return'$(favanimal)'train'
+        
+    }
+    if (roadtype === 'court'){
+        return'$(favanimal)'house'
+        
+    }
+    // lastname
+    function genlastname(lastname){
+        const lastleter = lastname.charAt(lastname.length-1)
+
+
+
+
+
+    }
+
+    // function of all functions 
+    genfullname
+
+
+
+
+
+    const prefix = genprefix (firstname)
+    const 
+    const 
+    const
+    const 
